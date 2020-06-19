3c4ac9a3612d8da8a9fa4e3a013b8720ae2db806
techtino <36424643+techtino@users.noreply.github.com>
2020-06-19 19:21:59 +0100

    Update changetheme.sh

--- .config/wal/changetheme.sh
+++ .config/wal/changetheme.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-wget --no-check-certificate -O ./wallpaper.png https://source.unsplash.com/1920x1080/
-wpg -s ~/.config/wal/wallpaper.png
-feh --bg-scale ~/.config/wal/wallpaper.png
+rm ~/.config/wal/*.png
+
+wallpapernum=$RANDOM
+
+wget --no-check-certificate -O ~/.config/wal/wall$wallpapernum.png https://source.unsplash.com/1920x1080/
+wpg -s ~/.config/wal/wall$wallpapernum.png
+feh --bg-scale ~/.config/wal/wall$wallpapernum.png
 ~/.config/wal/pywal-discord/pywal-discord
 pywalfox update
 spicetify update
